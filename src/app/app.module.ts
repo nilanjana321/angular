c32831f34adade92968846696679c3284a6e3433
nilanjana321 <nilanjana2k1@gmail.com>
2022-02-26 00:57:50 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { customerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,customerComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
